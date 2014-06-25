7a9d15b514133016b9d33e5d1155db8cd9d4c61d
WurzelPurzel <jannickbalsies@gmail.com>
2014-06-25 16:51:26 +0200

    Auskommentierte SwitchLayer Funktion hinzugefügt

--- VergissMeinNicht/VergissMeinNicht/Entities/TheodorChild.cs
+++ VergissMeinNicht/VergissMeinNicht/Entities/TheodorChild.cs
@@ -98,8 +98,46 @@ namespace VergissMeinNicht.Entities
 		private void CustomActivity()
 		{
             AnimationActivity();
+            //SwitchLayer();
+		}        
 
-		}
+        /*private void SwitchLayer()
+        {
+            float SpriteDiff = 0.001f;
+            float CollisionHeightDiff = 0.15f;
+            float CollisionWidthDiff = 0.08f;
+
+            if (this.SpriteInstance.TextureScale > 0.8)
+            {
+                switch (DirectionFacing)
+                {
+                    case LeftOrRight.Left:
+                        this.SpriteInstance.CurrentChainName = "IdleLeft";
+                        this.SpriteInstance.AnimationSpeed -= SpriteDiff;
+                        this.SpriteInstance.TextureScale -= SpriteDiff;
+                        base.Collision.Height -= CollisionHeightDiff;
+                        base.Collision.Width -= CollisionWidthDiff;
+                        InputManager.Keyboard.IgnoreKeyForOneFrame(Keys.Up);
+                        InputManager.Keyboard.IgnoreKeyForOneFrame(Keys.Down);
+                        InputManager.Keyboard.IgnoreKeyForOneFrame(Keys.Left);
+                        InputManager.Keyboard.IgnoreKeyForOneFrame(Keys.Right);
+                        break;
+
+                    case LeftOrRight.Right:
+                        this.SpriteInstance.CurrentChainName = "IdleRight";
+                        this.SpriteInstance.AnimationSpeed -= SpriteDiff;
+                        this.SpriteInstance.TextureScale -= SpriteDiff;
+                        base.Collision.Height -= CollisionHeightDiff;
+                        base.Collision.Width -= CollisionWidthDiff;
+                        InputManager.Keyboard.IgnoreKeyForOneFrame(Keys.Up);
+                        InputManager.Keyboard.IgnoreKeyForOneFrame(Keys.Down);
+                        InputManager.Keyboard.IgnoreKeyForOneFrame(Keys.Left);
+                        InputManager.Keyboard.IgnoreKeyForOneFrame(Keys.Right);
+                        break;
+                }
+            }
+            
+        }*/
 
 		private void CustomDestroy()
 		{
